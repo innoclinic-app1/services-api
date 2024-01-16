0c389339acf45cc812b3353f420dbd2f3987434e
Andrew Shumel <155365407+capybo@users.noreply.github.com>
2024-01-16 11:15:27 +0300

    Added domain entities

---
+++ src/Domain/Entities/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+public class Service
+{
+    public int Id { get; set; }
+    public int CategoryId { get; set; }
+    public int SpecializationId { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ src/Domain/Entities/Specialization.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class Specialization
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public bool IsActive { get; set; }
+}
